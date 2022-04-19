c59978c9a1ee3c6819123a8b9676b322a3c66708
Anna <anmaklos@gmail.com>
2022-04-19 15:40:47 +0200

    ready to go

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# github-finder
+React app to searching profiles on GitHub with GitHub API
+
+Styling:
+
+- tailwind
+- daisyui
+- react-icons
+
+React-Router-Dom v.6
+
+Axios
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,38 @@
+import { FaGithub } from "react-icons/fa";
+import { Link, NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
+
+function Navbar({ title }) {
+  return (
+    <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
+      <div className="container mx-auto">
+        <div className="flex-none px-2 mx-2">
+          <FaGithub className="inline pr-2 text-3xl" />
+          <Link to="/" className="text-lg font-bold align-middle">
+            {title}
+          </Link>
+        </div>
+        <div className="flex-1 px-2 mx-2">
+          <div className="flex justify-end">
+            <NavLink to="/" className="btn btn-ghost btn-sm rounded-btn ">
+              Home
+            </NavLink>
+            <NavLink to="/about" className="btn btn-ghost btn-sm rounded-btn">
+              About
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+Navbar.defaultProps = {
+  title: "GitHub Finder",
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string,
+};
+
+export default Navbar;
